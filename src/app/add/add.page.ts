b705a130bafb3eaff9f6db7b7d58fdc5c49d95fb
arielcoulson <ariel.coulson98@gmail.com>
2019-11-13 18:14:39 -0500

    reactive form stuff

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-add',
   templateUrl: './add.page.html',
@@ -7,9 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPage implements OnInit {
 
-  constructor() { }
+  addForm: FormGroup;
+
+  error_messages = {
+    'email': [
+      {type: 'required', message: 'Email is required.'},
+      {type: 'minLength', message: 'min length is 6'}
+    ],
+
+  }
+  constructor(
+    public formBuilder: FormBuilder
+  ) {
+      this.addForm = this.formBuilder.group({
+        email: new FormControl('',Validators.compose([
+          Validators.required
+        ])),
+        company: new FormControl('',Validators.compose([
+          Validators.required
+        ])),
+        jobTitle: new FormControl(''),
+        jobDescription: new FormControl(''),
+        jobLink: new FormControl(''),
+        phone: new FormControl(''),
+        status: new FormControl(''),
+        dateApplied: new FormControl(''),
+        dateInterview: new FormControl(''),
+        locationInterview: new FormControl(''),
+        notesInterview: new FormControl(''),
+        dateOffer: new FormControl(''),
+        amountOffer: new FormControl('') 
+      });
+   }
 
   ngOnInit() {
   }
 
+  isInterview(){
+    if(this.addForm.value.status == 'interview'){
+      console.log("interview");
+      return true;
+    }
+    return false;
+  }
+
+  isOffer(){
+    if(this.addForm.value.status == 'offer'){
+      return true;
+    }
+    return false;
+  }
+
+  add(){
+    console.log('email: ', this.addForm.value.email);
+  }
 }
