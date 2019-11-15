3d63009c714008d05cd3c84acc555e67e17bb535
bradleywyros <bradleywyros@gmail.com>
2019-11-14 23:35:01 -0500

    Fixed conflict... I think

---
+++ src/app/add/add.page.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.page.html',
+  styleUrls: ['./add.page.scss'],
+})
+export class AddPage implements OnInit {
+
+  addForm: FormGroup;
+
+  error_messages = {
+    'email': [
+      {type: 'required', message: 'Email is required.'},
+      {type: 'minLength', message: 'min length is 6'}
+    ],
+
+  }
+  constructor(
+    public formBuilder: FormBuilder
+  ) {
+      this.addForm = this.formBuilder.group({
+        email: new FormControl('',Validators.compose([
+          Validators.required
+        ])),
+        company: new FormControl('',Validators.compose([
+          Validators.required
+        ])),
+        jobTitle: new FormControl(''),
+        jobDescription: new FormControl(''),
+        jobLink: new FormControl(''),
+        phone: new FormControl(''),
+        status: new FormControl(''),
+        dateApplied: new FormControl(''),
+        dateInterview: new FormControl(''),
+        locationInterview: new FormControl(''),
+        notesInterview: new FormControl(''),
+        dateOffer: new FormControl(''),
+        amountOffer: new FormControl('') 
+      });
+   }
+
+  ngOnInit() {
+  }
+
+  isInterview(){
+    if(this.addForm.value.status == 'interview'){
+      console.log("interview");
+      return true;
+    }
+    return false;
+  }
+
+  isOffer(){
+    if(this.addForm.value.status == 'offer'){
+      return true;
+    }
+    return false;
+  }
+
+  add(){
+    console.log('email: ', this.addForm.value.email);
+  }
+}
