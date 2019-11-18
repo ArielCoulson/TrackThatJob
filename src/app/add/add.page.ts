38d3709d3c3305b264f7dc75560a25a326f2b933
arielcoulson <ariel.coulson98@gmail.com>
2019-11-18 13:33:45 -0500

    added conditional statements to avoid undefined errors

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ApplicationService, Application } from 'src/app/services/application.service'
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { LoadingController, AlertController } from '@ionic/angular';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { IfStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-add',
@@ -12,6 +15,8 @@ export class AddPage implements OnInit {
 
   addForm: FormGroup;
   private application: Application;
+  private applicationCollection: AngularFirestoreCollection<Application>;
+  private user: Observable<firebase.User>;
 
   error_messages = {
     'email': [
@@ -20,11 +25,9 @@ export class AddPage implements OnInit {
     ],
 
   }
-  constructor(public loadingCtrl: LoadingController, public formBuilder: FormBuilder,public applicationService: ApplicationService) {
+  constructor(public loadingCtrl: LoadingController, public formBuilder: FormBuilder,public applicationService: ApplicationService, private afs: AngularFirestore) {
       this.addForm = this.formBuilder.group({
-        email: new FormControl('',Validators.compose([
-          Validators.required
-        ])),
+        email: new FormControl(''),
         company: new FormControl('',Validators.compose([
           Validators.required
         ])),
@@ -61,33 +64,39 @@ export class AddPage implements OnInit {
     return false;
   }
 
-  async add(){
-    const loading = await this.loadingCtrl.create();
+  add(){
+    //const loading = await this.loadingCtrl.create();
 
-    //weird issue existing with the date field. 
+    //sorry this is ugly Ill clean it up
     var defaultDate = new Date('1995-12-17T03:24:00');
     const theApplication = {} as Application;
     theApplication.company = this.addForm.value.company;
     theApplication.job_title = this.addForm.value.jobTitle;
     theApplication.contact = {email: "", phone:""};
-    theApplication.contact.email = "test";
-    theApplication.contact.phone = "test";
+    theApplication.contact.email = this.addForm.value.email;
+    theApplication.contact.phone = this.addForm.value.phone;
     theApplication.description = this.addForm.value.jobDescription;
     theApplication.link = this.addForm.value.jobLink;
     theApplication.status = this.addForm.value.status;
     theApplication.date_applied = this.addForm.value.dateApplied;
-    //theApplication.status_info = {interview: {date: defaultDate,location:"", notes:""}, offer:{amount:"",date:defaultDate}};
-    theApplication.status_info.interview.date = this.addForm.value.dateInterviewed;
-    theApplication.status_info.interview.location = this.addForm.value.loationInterview;
-    theApplication.status_info.interview.notes = this.addForm.value.notesInterview;
-    theApplication.status_info.offer.amount = this.addForm.value.amountOffer;
-    //theApplication.status_info.offer.date = this.addForm.value.dateOffer;
+    theApplication.status_info = {interview:{date:defaultDate,location:"",notes:""},offer:{accept_by:defaultDate,amount:""}};
 
+    if(this.addForm.value.dateInterview != undefined)
+      theApplication.status_info.interview.date = this.addForm.value.dateInterview;
+    
+    if(this.addForm.value.loationInterview != undefined)
+      theApplication.status_info.interview.location = this.addForm.value.locationInterview;
+     
+    if(this.addForm.value.notesInterview != undefined)
+      theApplication.status_info.interview.notes = this.addForm.value.notesInterview;
+    
+    if(this.addForm.value.amountOffer != undefined)
+      theApplication.status_info.offer.amount = this.addForm.value.amountOffer;
+    
+    if(this.addForm.value.dateOffer != undefined)
+      theApplication.status_info.offer.accept_by = this.addForm.value.dateOffer;
 
-    this.applicationService.addApplication(theApplication);
-
-
-    return await loading.present();
-    console.log('email: ', this.addForm.value.email);
+    this.applicationCollection = this.afs.collection('users').doc('nlW6XvYgazNtRxkREsaB').collection('applications');
+    this.applicationCollection.add(theApplication);
   }
 }
