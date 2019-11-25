4c9594295d321a2a00b283d482fbe9533d9c75ad
brent cates <brentcates10@gmail.com>
2019-11-25 03:59:25 -0500

    added authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,13 +11,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
   { path: 'idea/:id', loadChildren: './pages/idea-details/idea-details.module#IdeaDetailsPageMod
  */
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  //{ path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'add', loadChildren: './add/add.module#AddPageModule' },
   { path: 'company/:id', loadChildren: './company/company.module#CompanyPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
   { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  
 ];
 
 @NgModule({
